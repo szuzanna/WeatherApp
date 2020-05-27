937c49c78bd3391af1cb93167f1d556783d7bb77
klaubudz <budzynska.klaudia@gmail.com>
2020-05-27 10:57:27 +0200

    first scene UI

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Student on 27/05/2020.
+//  Copyright © 2020 Zuzanna Smiech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Author_data: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
+∑
